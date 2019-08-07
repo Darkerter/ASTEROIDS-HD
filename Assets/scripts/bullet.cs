5281068cec7c8e2b31e80b955a2f83bc595519f5
darkerter <lacosa784@gmail.com>
2019-08-07 11:38:03 -0500

    asteriods alpha 0.1
    have made the basic scrpits for the player

---
+++ Assets/scripts/bullet.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+    public float BulletSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        BulletForce();
+        //destroys de object
+        Destroy(this.gameObject, 2);
+    }
+    void BulletForce()
+    {
+        transform.Translate((Vector2.up * Time.deltaTime) * BulletSpeed);
+
+        
+
+    }
+}
---
+++ Assets/scripts/movement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    float HorizontalM;
+    float VerticalM;
+
+     public int speed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+      
+    }
+
+   
+
+    // FixedUpdate is called once per frame fixed- for physics
+    void FixedUpdate()
+    {
+        HorizontalM = Input.GetAxisRaw("Horizontal");
+        VerticalM  = Input.GetAxisRaw("Vertical");
+        transform.Translate(new Vector3(HorizontalM * Time.fixedDeltaTime, VerticalM * Time.fixedDeltaTime)*speed);
+    }
+
+
+}
